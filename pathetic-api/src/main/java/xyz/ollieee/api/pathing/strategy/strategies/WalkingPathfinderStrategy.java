02d6114c016e3eb4857d0047251cd6fc3ee1d85f
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-03-24 16:47:22 +0000

    Fix java 8

--- pathetic-api/src/main/java/xyz/ollieee/api/pathing/strategy/strategies/WalkingPathfinderStrategy.java
+++ pathetic-api/src/main/java/xyz/ollieee/api/pathing/strategy/strategies/WalkingPathfinderStrategy.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import xyz.ollieee.api.pathing.strategy.PathfinderStrategy;
 import xyz.ollieee.api.wrapper.PathBlock;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class WalkingPathfinderStrategy implements PathfinderStrategy {
 
         if (previous == null && previouser == null) return blockIsValid(current);
 
-        List<Integer> heights = List.of(current.getBlockY(), previous.getBlockY(), previouser.getBlockY());
+        List<Integer> heights = Arrays.asList(current.getBlockY(), previous.getBlockY(), previouser.getBlockY());
 
         boolean currentIsValid = this.blockIsValid(current);
         boolean previousIsValid = this.blockIsValid(previous);
@@ -28,13 +29,13 @@ public class WalkingPathfinderStrategy implements PathfinderStrategy {
 
         if (dy == 1) {
             // This means that two of the locations are valid, which allows for a jump of one block
-            return (previouserIsValid || Collections.frequency(List.of(currentIsValid, previousIsValid, previouserIsValid), true) >= 2) && current.isPassable();
+            return (previouserIsValid || Collections.frequency(Arrays.asList(currentIsValid, previousIsValid, previouserIsValid), true) >= 2) && current.isPassable();
         }else if (dy >= 2) {
             // Change in height of more than 1 means its impossible to traverse
             return false;
         }
         // Didn't change height
-        return (previouserIsValid || Collections.frequency(List.of(currentIsValid, previousIsValid, previouserIsValid), true) >= 2) && current.isPassable();
+        return (previouserIsValid || Collections.frequency(Arrays.asList(currentIsValid, previousIsValid, previouserIsValid), true) >= 2) && current.isPassable();
     }
 
     private boolean blockIsValid(PathBlock block) {
