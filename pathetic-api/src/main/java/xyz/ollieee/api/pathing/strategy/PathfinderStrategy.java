4eaba57ed3486ff5f559c085593d9d2898f5583b
Luziferium <luziferisdoof@gmail.com>
2022-07-15 03:20:59 +0200

    Mentioned that previous and "previouser" can be null

--- pathetic-api/src/main/java/xyz/ollieee/api/pathing/strategy/PathfinderStrategy.java
+++ pathetic-api/src/main/java/xyz/ollieee/api/pathing/strategy/PathfinderStrategy.java
@@ -13,8 +13,8 @@ public interface PathfinderStrategy {
      * Implement the logic to see if a given location is valid for a strategy
      *
      * @param current The current {@link PathBlock} to check
-     * @param previous The previous {@link PathBlock} to check
-     * @param previouser The {@link PathBlock} before the previous to check
+     * @param previous The previous {@link PathBlock} to check or null if not available
+     * @param previouser The {@link PathBlock} before the previous to check or null if not available
      */
     boolean isValid(@NonNull PathBlock current, PathBlock previous, PathBlock previouser);
 
