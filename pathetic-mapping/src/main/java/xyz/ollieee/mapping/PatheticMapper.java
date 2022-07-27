4db4426982e371bd6a243ceadbfac9bb90c5b6a7
Luziferium <luziferisdoof@gmail.com>
2022-07-27 13:30:10 +0200

    Made PatheticMapper a UtilityClass

--- pathetic-mapping/src/main/java/xyz/ollieee/mapping/PatheticMapper.java
+++ pathetic-mapping/src/main/java/xyz/ollieee/mapping/PatheticMapper.java
@@ -1,6 +1,7 @@
 package xyz.ollieee.mapping;
 
 import lombok.NonNull;
+import lombok.experimental.UtilityClass;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.ollieee.Pathetic;
 import xyz.ollieee.api.pathing.Pathfinder;
@@ -8,7 +9,8 @@ import xyz.ollieee.api.snapshot.MaterialParser;
 import xyz.ollieee.api.snapshot.SnapshotManager;
 import xyz.ollieee.model.pathing.PathfinderImpl;
 
-public final class PatheticMapper {
+@UtilityClass
+public class PatheticMapper {
 
     /**
      * Initializes the Lib. If the lib is not initialized yet but is used anyways, this will lead to many boooooms.
@@ -16,23 +18,20 @@ public final class PatheticMapper {
      * @throws IllegalStateException If an attempt is made to initialize more than 1 time
      * @param javaPlugin the JavaPlugin which initializes the lib
      */
-    public static void initialize(JavaPlugin javaPlugin) {
+    public void initialize(JavaPlugin javaPlugin) {
         Pathetic.initialize(javaPlugin);
     }
 
-    public static @NonNull SnapshotManager getSnapshotManager() {
+    public @NonNull SnapshotManager getSnapshotManager() {
         return Pathetic.getSnapshotManager();
     }
 
-    public static @NonNull MaterialParser getMaterialParser() {
+    public @NonNull MaterialParser getMaterialParser() {
         return Pathetic.getMaterialParser();
     }
 
-    public static @NonNull Pathfinder newPathfinder() {
+    public @NonNull Pathfinder newPathfinder() {
         return new PathfinderImpl();
-    }
-
-    private PatheticMapper() {
     }
 
 }
