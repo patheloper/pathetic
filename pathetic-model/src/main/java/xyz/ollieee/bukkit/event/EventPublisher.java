05f51785bd618fec520bed11795ea181ee882f7b
Luziferium <luziferisdoof@gmail.com>
2022-07-27 12:05:33 +0200

    Removed EventUtil class, Now EventPublisher only calls events sync

--- pathetic-model/src/main/java/xyz/ollieee/bukkit/event/EventPublisher.java
+++ pathetic-model/src/main/java/xyz/ollieee/bukkit/event/EventPublisher.java
@@ -1,11 +1,18 @@
 package xyz.ollieee.bukkit.event;
 
 import org.bukkit.Bukkit;
+import xyz.ollieee.Pathetic;
 import xyz.ollieee.api.event.PathingEvent;
 
 public class EventPublisher {
 
     public static void raiseEvent(PathingEvent pathingEvent) {
+
+        if(!Bukkit.isPrimaryThread()) {
+            Bukkit.getScheduler().runTask(Pathetic.getPluginInstance(), () -> raiseEvent(pathingEvent));
+            return;
+        }
+
         Bukkit.getPluginManager().callEvent(pathingEvent);
     }
 
