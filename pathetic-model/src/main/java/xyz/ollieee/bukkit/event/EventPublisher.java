b8917812ca41964eab048b77ab19a43fc3159cb0
Luziferium <luziferisdoof@gmail.com>
2022-07-28 10:10:37 +0200

    Made EventPublisher a utility class

--- pathetic-model/src/main/java/xyz/ollieee/bukkit/event/EventPublisher.java
+++ pathetic-model/src/main/java/xyz/ollieee/bukkit/event/EventPublisher.java
@@ -1,9 +1,11 @@
 package xyz.ollieee.bukkit.event;
 
+import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 import xyz.ollieee.Pathetic;
 import xyz.ollieee.api.event.PathingEvent;
 
+@UtilityClass
 public class EventPublisher {
 
     public static void raiseEvent(PathingEvent pathingEvent) {
