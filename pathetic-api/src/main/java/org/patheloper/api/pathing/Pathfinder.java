26375ca08aa4e50b839fde33aef3069a19e2fa3e
Luziferium <luziferisdoof@gmail.com>
2022-11-25 11:04:08 +0100

    Changed internal model design around the pathfinder to be able to expand.
    NOTE: This also affects the public api in some way (f.e. the return type of the #findPath method in Pathfinder.java)

--- pathetic-api/src/main/java/org/patheloper/api/pathing/Pathfinder.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/Pathfinder.java
@@ -1,16 +1,21 @@
 package org.patheloper.api.pathing;
 
 import lombok.NonNull;
-import org.patheloper.api.pathing.result.task.PathingTask;
+import org.patheloper.api.pathing.result.PathfinderResult;
 import org.patheloper.api.wrapper.PathLocation;
 
+import java.util.concurrent.CompletionStage;
+
+/**
+ * A Pathfinder is a class that can find a path between two locations while following a given set of rules.
+ */
 public interface Pathfinder {
 
     /**
      * Tries to find a Path between the two {@link PathLocation}'s provided.
      *
-     * @return {@link PathingTask} the the task object
+     * @return An {@link CompletionStage} that will contain a {@link PathfinderResult}.
      */
     @NonNull
-    PathingTask findPath(@NonNull PathLocation start, @NonNull PathLocation target);
+    CompletionStage<PathfinderResult> findPath(@NonNull PathLocation start, @NonNull PathLocation target);
 }
---
+++ pathetic-model/src/main/java/org/patheloper/model/pathing/Offset.java
@@ -0,0 +1,33 @@
+package org.patheloper.model.pathing;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.patheloper.api.wrapper.PathVector;
+
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+public enum Offset {
+
+    BLOCK_BY_BLOCK(new PathVector[] {
+            new PathVector(1, 0, 0),
+            new PathVector(-1, 0, 0),
+            new PathVector(0, 0, 1),
+            new PathVector(0, 0, -1),
+            new PathVector(0, 1, 0),
+            new PathVector(0, -1, 0),
+    }),
+    DIAGONAL(new PathVector[] {
+            new PathVector(1, 0, 1), new PathVector(-1, 0, -1), new PathVector(-1, 0, 1), new PathVector(1, 0, -1),
+            new PathVector(0, 1, 1), new PathVector(1, 1, 1), new PathVector(1, 1, 0), new PathVector(1, 1, -1),
+            new PathVector(0, 1, -1), new PathVector(-1, 1, -1), new PathVector(-1, 1, 0), new PathVector(-1, 1, 1),
+            new PathVector(0, -1, 1), new PathVector(1, -1, 1), new PathVector(1, -1, 0), new PathVector(1, -1, -1),
+            new PathVector(0, -1, -1), new PathVector(-1, -1, -1), new PathVector(-1, -1, 0), new PathVector(-1, -1, 1),
+    }),
+    MERGED(Stream.concat(Stream.of(BLOCK_BY_BLOCK.getVectors()), Stream.of(DIAGONAL.getVectors()))
+            .toArray(PathVector[]::new));
+
+    @Getter
+    private final PathVector[] vectors;
+
+}
