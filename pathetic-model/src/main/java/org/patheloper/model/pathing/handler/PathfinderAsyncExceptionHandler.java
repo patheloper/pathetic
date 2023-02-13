186f8586cc356d285763eb3c855b77ba88f8102f
Luziferium <luziferisdoof@gmail.com>
2023-02-13 13:34:53 +0100

    Now using the lombok annotation to log like a cool kid 😎

--- pathetic-model/src/main/java/org/patheloper/model/pathing/handler/PathfinderAsyncExceptionHandler.java
+++ pathetic-model/src/main/java/org/patheloper/model/pathing/handler/PathfinderAsyncExceptionHandler.java
@@ -1,14 +1,12 @@
 package org.patheloper.model.pathing.handler;
 
-import org.apache.logging.log4j.Logger;
-import static org.apache.logging.log4j.LogManager.getLogger;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class PathfinderAsyncExceptionHandler implements Thread.UncaughtExceptionHandler {
-
-    private static final Logger LOGGER = getLogger(PathfinderAsyncExceptionHandler.class);
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        LOGGER.error("Exception caught in PathfinderAsyncHandler:", e);
+        log.error("Exception caught in PathfinderAsyncHandler:", e);
     }
 }
