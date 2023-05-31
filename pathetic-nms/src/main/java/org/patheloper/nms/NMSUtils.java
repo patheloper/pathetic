6376aef89299126b7562a06f4947a3807bce9f51
Lukas Planz <lukas.planz@web.de>
2023-05-31 16:56:54 +0200

    Remove Paper implementation and alter NMSUtils to avoid minimizing issues

--- pathetic-nms/src/main/java/org/patheloper/nms/NMSUtils.java
+++ pathetic-nms/src/main/java/org/patheloper/nms/NMSUtils.java
@@ -1,42 +1,40 @@
 package org.patheloper.nms;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.patheloper.api.snapshot.NMSInterface;
 
 public class NMSUtils {
+
+    private static final Class<?>[] supportedVersions = new Class[]{
+            NMSInterfacev1_8_R3.class,
+            NMSInterfacev1_12_R1.class,
+            NMSInterfacev1_15_R1.class,
+            NMSInterfacev1_16_R3.class,
+            NMSInterfacev1_17_R1.class,
+            NMSInterfacev1_18_R1.class,
+            NMSInterfacev1_19_R2.class,
+            NMSInterfacev1_19_R3.class
+    };
 
     private final NMSInterface nmsInterface;
 
     public NMSUtils() {
         String nmsVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-        if (supportsAsyncLoading()) {
-            nmsInterface = new NMSInterfacePaper();
-        } else {
-            try {
-                nmsInterface =
-                        (NMSInterface) Class.forName(getClass().getPackage().getName() + ".NMSInterface" + nmsVersion)
-                                .getConstructor()
-                                .newInstance();
-            } catch (ClassNotFoundException e) {
-                throw new IllegalArgumentException("Unsupported version: " + nmsVersion, e);
-            } catch (ReflectiveOperationException e) {
-                throw new IllegalArgumentException("Could not create NMSInterface for version " + nmsVersion, e);
+        try {
+            for (Class<?> clazz : supportedVersions) {
+                if (clazz.getSimpleName().endsWith(nmsVersion)) {
+                    nmsInterface = (NMSInterface) clazz.getConstructor().newInstance();
+                    return;
+                }
             }
+            throw new IllegalArgumentException("Unsupported version: " + nmsVersion);
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalArgumentException("Could not create NMSInterface for version " + nmsVersion, e);
         }
     }
 
     public NMSInterface getNmsInterface() {
         return this.nmsInterface;
-    }
-
-    private boolean supportsAsyncLoading() {
-        try {
-            World.class.getDeclaredMethod("getChunkAtAsyncUrgently", int.class, int.class);
-            return true;
-        } catch (NoSuchMethodException e) {
-            return false;
-        }
     }
 }
