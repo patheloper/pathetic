ec1e106b8dbc7f9252c7f3782fd2058642871bdc
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-07-05 11:02:00 +0100

    Add some QOL methods

--- pathetic-api/src/main/java/org/patheloper/api/pathing/result/PathState.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/result/PathState.java
@@ -21,6 +21,25 @@ public enum PathState {
     /**
      * Signifies that the pathfinder reached its iteration limit
      */
-    MAX_ITERATIONS_REACHED,
+    MAX_ITERATIONS_REACHED;
+
+    /**
+     * Whether the pathfinder has failed to reach its target. This includes
+     * {@link #FAILED}, {@link #LENGTH_LIMITED}, {@link #MAX_ITERATIONS_REACHED} and {@link #FALLBACK}
+     *
+     * @return Whether the pathfinder has failed to reach its target
+     */
+    public boolean hasFailed() {
+        return this == FAILED || this == LENGTH_LIMITED || this == MAX_ITERATIONS_REACHED || this == FALLBACK;
+    }
+
+    /**
+     * Whether the pathfinder has successfully reached its target. This includes {@link #FOUND}
+     *
+     * @return Whether the pathfinder has successfully reached its target
+     */
+    public boolean wasSuccessful() {
+        return this == FOUND;
+    }
 
 }
