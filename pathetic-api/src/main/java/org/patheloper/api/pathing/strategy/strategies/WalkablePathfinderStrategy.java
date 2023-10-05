02358c6ece8fd4d6c46febf1aaf964eac176f05c
Luziferium <luziferisdoof@gmail.com>
2023-10-05 04:55:07 +0200

    unfucked WalkablePathfinderStrategy

--- pathetic-api/src/main/java/org/patheloper/api/pathing/strategy/strategies/WalkablePathfinderStrategy.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/strategy/strategies/WalkablePathfinderStrategy.java
@@ -13,8 +13,6 @@ public class WalkablePathfinderStrategy implements PathfinderStrategy {
 
     private final int height;
 
-    private PathPosition lastExamined;
-
     public WalkablePathfinderStrategy() {
         this(2);
     }
@@ -26,63 +24,21 @@ public class WalkablePathfinderStrategy implements PathfinderStrategy {
     @Override
     public boolean isValid(@NonNull PathPosition position, @NonNull SnapshotManager snapshotManager) {
         PathBlock block = snapshotManager.getBlock(position);
-        PathBlock blockBelow = snapshotManager.getBlock(position.add(0, -1, 0));
-
-        boolean canStandOnIt = canStandOnBlock(position, snapshotManager, block, blockBelow);
-
-        if (canStandOnIt) {
-            this.lastExamined = position;
-            return true;
-        }
-
-        if (lastExamined != null) {
-            boolean withinDistance = isWithinDistance(lastExamined, position);
-            boolean isHigher = isHigherPosition(lastExamined, position);
-            boolean isLower = isLowerPosition(lastExamined, position);
-            boolean isPassable = isPassableBlock(block, snapshotManager, areBlocksAbovePassable(position, snapshotManager));
-
-            return withinDistance && (isHigher || isLower) && isPassable;
-        }
-
-        return false;
+        return canStandOn(block, snapshotManager);
     }
-
-    private boolean canStandOnBlock(PathPosition position, SnapshotManager snapshotManager, PathBlock block, PathBlock blockBelow) {
-        boolean areBlocksAbovePassable = areBlocksAbovePassable(position, snapshotManager);
-
-        for (int i = 1; i < height; i++) {
-            PathBlock blockAbove = snapshotManager.getBlock(position.add(0, i, 0));
-            if (!blockAbove.isPassable()) {
-                areBlocksAbovePassable = false;
-                break;
-            }
-        }
-
-        return block.isPassable() && blockBelow.isSolid() && areBlocksAbovePassable;
-    }
-
-    private boolean isWithinDistance(PathPosition lastExamined, PathPosition position) {
-        double distance = lastExamined.distance(position);
-        return distance <= 3;
-    }
-
-    private boolean isHigherPosition(PathPosition lastExamined, PathPosition position) {
-        return position.getY() - lastExamined.getY() > 2;
-    }
-
-    private boolean isLowerPosition(PathPosition lastExamined, PathPosition position) {
-        return lastExamined.getY() - position.getY() > 2;
-    }
-
-    private boolean isPassableBlock(PathBlock block, SnapshotManager snapshotManager, boolean areBlocksAbovePassable) {
-        return block.isPassable() && areBlocksAbovePassable;
+    
+    private boolean canStandOn(PathBlock block, SnapshotManager snapshotManager) {
+        PathBlock below = snapshotManager.getBlock(block.getPathPosition().add(0, -1, 0));
+        return areBlocksAbovePassable(block.getPathPosition(), snapshotManager)
+                && block.isPassable()
+                && below.isSolid();
     }
 
     private boolean areBlocksAbovePassable(PathPosition position, SnapshotManager snapshotManager) {
         boolean areBlocksAbovePassable = true;
-        for (int i = 1; i < height; i++) {
-            PathBlock blockAbove = snapshotManager.getBlock(position.add(0, i, 0));
-            if (!blockAbove.isPassable()) {
+        for(int i = 1; i <= height; i++) {
+            PathBlock block = snapshotManager.getBlock(position.add(0, i, 0));
+            if(!block.isPassable()) {
                 areBlocksAbovePassable = false;
                 break;
             }
