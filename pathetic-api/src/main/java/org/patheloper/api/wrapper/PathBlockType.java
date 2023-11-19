bf76111cc6e7e5b2b8fdf920d5b76e2d822eff26
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-11-19 23:53:58 +0000

    Change the equals

--- pathetic-api/src/main/java/org/patheloper/api/wrapper/PathBlockType.java
+++ pathetic-api/src/main/java/org/patheloper/api/wrapper/PathBlockType.java
@@ -1,6 +1,6 @@
 package org.patheloper.api.wrapper;
 
-import lombok.EqualsAndHashCode;
+import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.Value;
 import org.bukkit.Material;
@@ -10,20 +10,23 @@ import org.bukkit.Material;
  */
 @Getter
 @Value
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class PathBlockType {
 
-    @EqualsAndHashCode.Include
     Material material;
     PathBlockCondition condition;
-    
+
     public PathBlockType(Material material) {
         this.material = material;
         this.condition = PathBlockCondition.fromMaterial(material);
     }
-    
+
+    public boolean equals(PathBlockType other) {
+        Preconditions.checkArgument(other != null, "Cannot compare to null");
+        return this.material == other.material;
+    }
+
     public enum PathBlockCondition {
-        
+
         /**
          * Represents an air block
          */
@@ -40,23 +43,23 @@ public class PathBlockType {
          * Represents all blocks that could be walked through, but are not explicitly air
          */
         OTHER;
-        
+
         /**
          * @return {@link PathBlockType} the block type of the given block
          */
         public static PathBlockCondition fromMaterial(Material material) {
-            
+
             if (material == Material.AIR) // can't check for cave or void air due to backwards compatibility
                 return AIR;
-            
+
             if(material.isSolid()) {
                 return SOLID;
             }
-            
+
             if(material == Material.LAVA || material == Material.WATER) {
                 return LIQUID;
             }
-            
+
             return OTHER;
         }
     }
