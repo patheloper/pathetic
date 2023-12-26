947a217a936f6bcdad534736e8aa291ebde37980
GodCipher <luziferisdoof@gmail.com>
2023-12-26 02:26:03 +0100

    Update JumpablePathfinderStrategy with non-nullable and distance validations
    This commit updates the JumpablePathfinderStrategy class to import non-null validations from the Checker Framework and rename the jumpDistance variable to maxJumpDistance for better context. It also enhances the isValid method by adding conditions to validate the jump within the allowed height and maximum distance.

--- pathetic-api/src/main/java/org/patheloper/api/pathing/strategy/strategies/JumpablePathfinderStrategy.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/strategy/strategies/JumpablePathfinderStrategy.java
@@ -1,6 +1,6 @@
 package org.patheloper.api.pathing.strategy.strategies;
 
-import lombok.NonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.patheloper.api.annotation.Experimental;
 import org.patheloper.api.snapshot.SnapshotManager;
 import org.patheloper.api.wrapper.PathBlock;
@@ -13,38 +13,40 @@ import org.patheloper.api.wrapper.PathPosition;
  */
 @Experimental
 public class JumpablePathfinderStrategy extends WalkablePathfinderStrategy {
-    
+
     private final int jumpHeight;
-    private final int jumpDistance;
-    
+    private final int maxJumpDistance;
+
     private PathPosition lastValidPosition = null;
-    
+
     public JumpablePathfinderStrategy() {
         this(2, 1, 4);
     }
-    
-    public JumpablePathfinderStrategy(int height, int jumpHeight, int jumpDistance) {
+
+    public JumpablePathfinderStrategy(int height, int jumpHeight, int maxJumpDistance) {
         super(height);
-        
+
         if(jumpHeight <= 0) throw new IllegalArgumentException("Jump height must be greater than 0");
-        if(jumpDistance <= 0) throw new IllegalArgumentException("Jump distance must be greater than 0");
-        
+        if(maxJumpDistance <= 0) throw new IllegalArgumentException("Jump distance must be greater than 0");
+
         this.jumpHeight = jumpHeight;
-        this.jumpDistance = jumpDistance;
+        this.maxJumpDistance = maxJumpDistance;
     }
-    
+
     @Override
-    public boolean isValid(@NonNull PathPosition position, @NonNull SnapshotManager snapshotManager) {
+    public boolean isValid(@org.checkerframework.checker.nullness.qual.NonNull PathPosition position, @NonNull SnapshotManager snapshotManager) {
+        if(lastValidPosition == null)
+            lastValidPosition = position;
+
         PathBlock startBlock = snapshotManager.getBlock(position);
         if(canStandOn(startBlock, snapshotManager)) {
             lastValidPosition = position;
             return true;
         }
-        
-        if(position.getBlockY() - lastValidPosition.getBlockY() > jumpHeight) {
+
+        if((position.getBlockY() - lastValidPosition.getBlockY()) > jumpHeight)
             return false;
-        }
-        
-        return !(position.distance(lastValidPosition) > jumpDistance);
+
+        return startBlock.isPassable() && !(position.distance(lastValidPosition) > maxJumpDistance);
     }
 }
