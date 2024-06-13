be02c00f952c936f9a6f2c18a2e5692679fe073f
GodCipher <luziferisdoof@gmail.com>
2024-06-13 12:26:17 +0200

    Renamed NMSInterface to ChunkDataProvider

---
+++ pathetic-nms/paper/src/main/java/org/patheloper/provider/paper/PaperChunkDataProvider.java
@@ -0,0 +1,24 @@
+package org.patheloper.provider.paper;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.World;
+import org.bukkit.block.BlockState;
+import org.patheloper.api.snapshot.ChunkDataProvider;
+
+public class PaperChunkDataProvider implements ChunkDataProvider {
+  
+  @Override
+  public ChunkSnapshot getSnapshot(World world, int chunkX, int chunkZ) {
+    if (Bukkit.isPrimaryThread()) {
+      return world.getChunkAt(chunkX, chunkZ).getChunkSnapshot();
+    } else {
+      return world.getChunkAtAsyncUrgently(chunkX, chunkZ).join().getChunkSnapshot();
+    }
+  }
+  
+  @Override
+  public BlockState getBlockState(ChunkSnapshot snapshot, int x, int y, int z) {
+    return snapshot.getBlockData(x, y, z).createBlockState();
+  }
+}
---
+++ pathetic-nms/v1_8/src/main/java/org/patheloper/provider/v1_8/OneEightChunkDataProviderImpl.java
@@ -0,0 +1,30 @@
+package org.patheloper.provider.v1_8;
+
+import net.minecraft.server.v1_8_R3.Chunk;
+import net.minecraft.server.v1_8_R3.WorldServer;
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.World;
+import org.bukkit.block.BlockState;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.patheloper.api.snapshot.ChunkDataProvider;
+
+public class OneEightChunkDataProviderImpl implements ChunkDataProvider {
+
+  @Override
+  public ChunkSnapshot getSnapshot(World world, int chunkX, int chunkZ) {
+    try {
+      WorldServer server = ((CraftWorld) world).getHandle();
+      Chunk chunk = server.chunkProviderServer.getChunkAt(chunkX, chunkZ);
+
+      return chunk.bukkitChunk.getChunkSnapshot();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public BlockState getBlockState(ChunkSnapshot snapshot, int x, int y, int z) {
+    return null;
+  }
+}
