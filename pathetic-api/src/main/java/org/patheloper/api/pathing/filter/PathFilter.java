b9d16c340101f3688daa416be7710f5a0c979486
GodCipher <luziferisdoof@gmail.com>
2024-06-25 16:33:37 +0200

    Further elaborated on PathFilter javadocs

--- pathetic-api/src/main/java/org/patheloper/api/pathing/filter/PathFilter.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/filter/PathFilter.java
@@ -7,6 +7,11 @@ import org.patheloper.api.pathing.Pathfinder;
  * A PathFilter is a functional interface that allows customization of the pathfinding process
  * within the {@link Pathfinder}. It provides a mechanism to influence the selection of paths during
  * the pathfinding algorithm's execution.
+ *
+ * <p>In essence, a PathFilter acts as a whitelist for blocks during the pathfinding process. It
+ * evaluates each potential block (or node) on the path and determines whether it is valid or not
+ * based on the implemented filter logic. Only the blocks that pass the filter are considered valid
+ * and included in the final path.
  */
 @FunctionalInterface
 public interface PathFilter {
