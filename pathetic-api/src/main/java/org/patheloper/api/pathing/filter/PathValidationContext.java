bd5079134c0a7c4324df021038813d51288743d3
GodCipher <luziferisdoof@gmail.com>
2024-09-25 23:55:43 +0200

    feat: improved javadocs

--- pathetic-api/src/main/java/org/patheloper/api/pathing/filter/PathValidationContext.java
+++ pathetic-api/src/main/java/org/patheloper/api/pathing/filter/PathValidationContext.java
@@ -4,11 +4,19 @@ import lombok.Value;
 import org.patheloper.api.snapshot.SnapshotManager;
 import org.patheloper.api.wrapper.PathPosition;
 
-/** A parameter object for the {@link PathFilter#filter} method. */
+/**
+ * A context object that holds relevant parameters for path validation, used by the {@link
+ * PathFilter#filter} method.
+ */
 @Value
 public class PathValidationContext {
 
+  /** The current {@link PathPosition} being validated in the pathfinding process. */
   PathPosition position;
+
+  /** The parent {@link PathPosition} from which the current position was reached. */
   PathPosition parent;
+
+  /** The {@link SnapshotManager} used to access historical state snapshots during validation. */
   SnapshotManager snapshotManager;
 }
